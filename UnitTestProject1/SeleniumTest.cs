db641f332efe9068770910c95578fe2f07ce8c87
NAVEED0BAB\Naveed <naveed.akhtar@psiog.com>
2018-08-16 11:24:38 +0530

    New Project

---
+++ UnitTestProject1/SeleniumTest.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using System.Threading;
+using UnitTestProject1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumTest
+{
+    public class SeleniumTest: Utilities 
+    {
+       
+        [SetUp]
+        public void Setup()
+        {
+            BrowserOptions();
+        }
+
+        [Test]
+        public void TestMethod1()
+        {   
+            driver.Url = "https://www.google.com/gmail/about/#";
+            driver.FindElement(By.LinkText("CREATE AN ACCOUNT")).Click();
+            Thread.Sleep(3000);
+        }
+
+
+        [Test]
+        public void TestMethod2()
+        {
+            driver.Url = "https://qaweb.empwr.com/";
+            Thread.Sleep(3000);
+        }
+
+        [TearDown]
+        public void teardown()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
+
---
+++ UnitTestProject1/Utilities.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    public class Utilities
+    {
+        public IWebDriver driver;
+
+        public void BrowserOptions()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("start-maximized");
+            options.AddArguments("--incognito");
+            driver = new ChromeDriver("C:/chromedriver_win32/", options);
+        }
+    }
+}
