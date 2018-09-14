3d7aa31ae4d5cab927ec0b62aede3e92ac73b67e
NAVEED0BAB\Naveed <naveed.akhtar@psiog.com>
2018-09-14 17:43:21 +0530

    Added new files for parameter

--- UnitTestProject1/SeleniumTest.cs
+++ UnitTestProject1/SeleniumTest.cs
@@ -10,17 +10,10 @@ namespace SeleniumTest
     public class SeleniumTest: Utilities 
     {
        
-        [SetUp]
-        public void Setup()
-        {
-            BrowserOptions();
-        }
-
         [Test]
         public void TestMethod1()
         {   
             driver.Url = "https://www.google.com/gmail/about/#";
-            driver.FindElement(By.LinkText("CREATE AN ACCOUNT")).Click();
             Thread.Sleep(3000);
         }
 
@@ -36,21 +29,10 @@ namespace SeleniumTest
         public void TestMethod3()
         {
             driver.Url = "https://www.news18.com/";
+            driver.FindElement(By.LinkText("CREATE AN ACCOUNT")).Click();
             Thread.Sleep(3000);
         }
 
-        [Test]
-        public void TestMethod4()
-        {
-            driver.Url = "https://www.news18.com/";
-            Thread.Sleep(3000);
-        }
-        [TearDown]
-        public void teardown()
-        {
-            driver.Close();
-            driver.Quit();
-        }
     }
 }
 
--- UnitTestProject1/Utilities.cs
+++ UnitTestProject1/Utilities.cs
@@ -1,8 +1,15 @@
-﻿using OpenQA.Selenium;
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,13 +18,102 @@ namespace UnitTestProject1
     public class Utilities
     {
         public IWebDriver driver;
+        protected ExtentReports _extent;
+        protected ExtentTest _test;
+        string ExecutionBrowser = System.Environment.GetEnvironmentVariable("Browser");
 
-        public void BrowserOptions()
+        [OneTimeSetUp]
+        protected void Setup()
         {
-            ChromeOptions options = new ChromeOptions();
-            options.AddArguments("start-maximized");
-            options.AddArguments("--incognito");
-            driver = new ChromeDriver("C:/chromedriver_win32/", options);
+            //var dir = "C:/Reports/";
+            //var fileName = this.GetType().ToString() + ".html";
+            var fileDirectory = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Reports\");
+            var htmlReporter = new ExtentHtmlReporter(fileDirectory+ this.GetType().ToString() + ".html");
+
+            _extent = new ExtentReports();
+            htmlReporter.LoadConfig(System.AppDomain.CurrentDomain.BaseDirectory + "extent-config.xml");
+            _extent.AttachReporter(htmlReporter);
+            KlovReporter klovReporter = new KlovReporter();
+            klovReporter.InitMongoDbConnection("localhost", 27017);
+            klovReporter.ProjectName = "Selenium Extent Report";
+            klovReporter.ReportName = "Build " + DateTime.Now.ToString();
+            klovReporter.KlovUrl = "http://localhost";
+            _extent.AttachReporter(klovReporter);
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            driver.Quit();
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            var stacktrace = string.IsNullOrEmpty(TestContext.CurrentContext.Result.StackTrace)
+                    ? ""
+                    : string.Format("{0}", TestContext.CurrentContext.Result.StackTrace);
+            Status logstatus;
+
+            switch (status)
+            {
+                case TestStatus.Failed:
+                    logstatus = Status.Fail;
+                    break;
+                case TestStatus.Inconclusive:
+                    logstatus = Status.Warning;
+                    break;
+                case TestStatus.Skipped:
+                    logstatus = Status.Skip;
+                    break;
+                default:
+                    logstatus = Status.Pass;
+                    break;
+            }
+
+            _test.Log(logstatus, "Test ended with " + logstatus + stacktrace);
+            _extent.Flush();
+        }
+
+        [OneTimeTearDown]
+        protected void TearDown()
+        {
+            _extent.Flush();
+        }
+        
+        [SetUp]
+        public IWebDriver GetWebDriverForBrowser()
+        {
+            IWebDriver driver = null;
+
+            switch (ExecutionBrowser)
+            {
+                case "Chrome":
+                    ChromeOptions options = new ChromeOptions();
+                    options.AddArguments("start-maximized");
+                    options.AddArguments("--incognito");
+                    driver = new ChromeDriver("C:/chromedriver_win32/", options);
+                    break;
+
+                case "Firefox":
+                    System.Environment.SetEnvironmentVariable("webdriver.gecko.driver", "C:/geckodriver-v0.21.0-win64/geckodriver.exe");
+                    driver = new FirefoxDriver();
+                    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMinutes(3);
+                    driver.Manage().Window.Maximize();
+                    break;
+
+                    //case Browser.IE:
+                    //    driver = new InternetExplorerDriver(@"C:\repos\Testing\Tests");
+                    //    break;
+            }
+            if (driver != null)
+            {
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMinutes(3);
+            }
+            _test = _extent.CreateTest(TestContext.CurrentContext.Test.Name);
+            return driver;
+        }
+        //ChromeOptions options = new ChromeOptions();
+        //options.AddArguments("start-maximized");
+        //options.AddArguments("--incognito");
+        //driver = new ChromeDriver("C:/chromedriver_win32/", options);
+       
         }
     }
-}
+
