6d3e6cfd2426b6212b5aa590ec61291f32b1623f
NAVEED0BAB\Naveed <naveed.akhtar@psiog.com>
2018-09-14 17:54:19 +0530

    Commented code

--- UnitTestProject1/Utilities.cs
+++ UnitTestProject1/Utilities.cs
@@ -27,12 +27,12 @@ namespace UnitTestProject1
         {
             //var dir = "C:/Reports/";
             //var fileName = this.GetType().ToString() + ".html";
-            var fileDirectory = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Reports\");
-            var htmlReporter = new ExtentHtmlReporter(fileDirectory+ this.GetType().ToString() + ".html");
+            //var fileDirectory = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Reports\");
+            //var htmlReporter = new ExtentHtmlReporter(fileDirectory+ this.GetType().ToString() + ".html");
 
-            _extent = new ExtentReports();
-            htmlReporter.LoadConfig(System.AppDomain.CurrentDomain.BaseDirectory + "extent-config.xml");
-            _extent.AttachReporter(htmlReporter);
+            //_extent = new ExtentReports();
+            //htmlReporter.LoadConfig(System.AppDomain.CurrentDomain.BaseDirectory + "extent-config.xml");
+            //_extent.AttachReporter(htmlReporter);
             //KlovReporter klovReporter = new KlovReporter();
             //klovReporter.InitMongoDbConnection("localhost", 27017);
             //klovReporter.ProjectName = "Selenium Extent Report";
@@ -45,36 +45,36 @@ namespace UnitTestProject1
         public void AfterTest()
         {
             driver.Quit();
-            var status = TestContext.CurrentContext.Result.Outcome.Status;
-            var stacktrace = string.IsNullOrEmpty(TestContext.CurrentContext.Result.StackTrace)
-                    ? ""
-                    : string.Format("{0}", TestContext.CurrentContext.Result.StackTrace);
-            Status logstatus;
+            //var status = TestContext.CurrentContext.Result.Outcome.Status;
+            //var stacktrace = string.IsNullOrEmpty(TestContext.CurrentContext.Result.StackTrace)
+            //        ? ""
+            //        : string.Format("{0}", TestContext.CurrentContext.Result.StackTrace);
+            //Status logstatus;
 
-            switch (status)
-            {
-                case TestStatus.Failed:
-                    logstatus = Status.Fail;
-                    break;
-                case TestStatus.Inconclusive:
-                    logstatus = Status.Warning;
-                    break;
-                case TestStatus.Skipped:
-                    logstatus = Status.Skip;
-                    break;
-                default:
-                    logstatus = Status.Pass;
-                    break;
-            }
+            //switch (status)
+            //{
+            //    case TestStatus.Failed:
+            //        logstatus = Status.Fail;
+            //        break;
+            //    case TestStatus.Inconclusive:
+            //        logstatus = Status.Warning;
+            //        break;
+            //    case TestStatus.Skipped:
+            //        logstatus = Status.Skip;
+            //        break;
+            //    default:
+            //        logstatus = Status.Pass;
+            //        break;
+            //}
 
-            _test.Log(logstatus, "Test ended with " + logstatus + stacktrace);
-            _extent.Flush();
+            //_test.Log(logstatus, "Test ended with " + logstatus + stacktrace);
+            //_extent.Flush();
         }
 
         [OneTimeTearDown]
         protected void TearDown()
         {
-            _extent.Flush();
+            //_extent.Flush();
         }
         
         [SetUp]
@@ -106,7 +106,7 @@ namespace UnitTestProject1
             {
                 driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMinutes(3);
             }
-            _test = _extent.CreateTest(TestContext.CurrentContext.Test.Name);
+            //_test = _extent.CreateTest(TestContext.CurrentContext.Test.Name);
             return driver;
         }
         //ChromeOptions options = new ChromeOptions();
