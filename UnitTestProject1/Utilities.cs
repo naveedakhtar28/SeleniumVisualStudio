b6c3f14b7b0d2a30116e4a483bb1bccfa993f619
NAVEED0BAB\Naveed <naveed.akhtar@psiog.com>
2018-09-18 15:18:47 +0530

    stage

--- UnitTestProject1/Utilities.cs
+++ UnitTestProject1/Utilities.cs
@@ -66,11 +66,12 @@ namespace UnitTestProject1
 
             var fileName = this.GetType().ToString() + ".html";
             var fileDirectory = "C:/Reports/";
+
             //var fileDirectory = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Reports\");
             var htmlReporter = new ExtentHtmlReporter(fileDirectory + this.GetType().ToString() + ".html");
 
             _extent = new ExtentReports();
-            //htmlReporter.LoadConfig("extent-config.xml");
+            htmlReporter.LoadConfig("extent-config.xml");
             _extent.AttachReporter(htmlReporter);
             //KlovReporter klovReporter = new KlovReporter();
             //klovReporter.InitMongoDbConnection("localhost", 27017);
